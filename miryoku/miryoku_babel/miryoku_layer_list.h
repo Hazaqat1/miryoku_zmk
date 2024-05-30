9fef0d37d9838e9b5a67aff5a89660e4c75d3f10
Hazaqat1 <Hazaqat.kiani@protonmail.com>
2024-05-31 00:09:17 +0100

    Update miryoku_layer_list.h

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -11,7 +11,6 @@ MIRYOKU_X(BASE,   "Base") \
 MIRYOKU_X(EXTRA,  "Extra") \
 MIRYOKU_X(TAP,    "Tap") \
 MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
 MIRYOKU_X(MOUSE,  "Mouse") \
 MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
@@ -22,11 +21,11 @@ MIRYOKU_X(FUN,    "Fun")
 #define U_EXTRA  1
 #define U_TAP    2
 #define U_BUTTON 3
-#define U_NAV    4
+//#define U_NAV    4
 #define U_MOUSE  5
 #define U_MEDIA  6
 #define U_NUM    7
 #define U_SYM    8
 #define U_FUN    9
-
+//MIRYOKU_X(NAV,    "Nav") \
 #endif
